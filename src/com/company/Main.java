cb40d0814b830fa21bdc8dd7b5b7ee36aa963b95
ilya <youdeaddark@gmail.com>
2016-06-30 23:03:31 +0500

    Added 2......56464

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello,Git!");
         for (int i=0;i<10;i++){
             System.out.println(i);
+            System.out.println(i+1);
         }
 
 
